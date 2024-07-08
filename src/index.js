d662c730ea161f8c58708a0a168fc295b9c87f1a
Adnan Hussain <hussainadnan531@gmail.com>
2024-07-08 12:57:42 +0530

    db connect changes

---
+++ src/index.js
@@ -0,0 +1,28 @@
+//  require(`dotenv`).config({path:`./env`})
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+ connectDB()
+
+
+
+
+
+
+
+
+
+ /*
+ ( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    } catch (error) {
+        console.error("Error : ", error)
+        throw error
+    }
+ })()
+ */
